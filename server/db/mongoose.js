c110e0e1213ff83c30693e6d8129b26ae6d28c67
Joseph Skrzysowski <josephskrzysowski@gmail.com>
2018-06-27 18:02:54 -0400

    Setup for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,8 +4,12 @@ const mongoose = require('mongoose');
 require('dotenv').config({ path: 'variables.env' });
 
 mongoose.Promise = global.Promise;
+// mongoose.connect(process.env.MONGODB_URI || process.env.DATABASE);
 mongoose.connect(process.env.MONGODB_URI || process.env.DATABASE);
 // mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 
 module.exports = { mongoose };
+
+
+/*MONGODB_URI: mongodb://heroku_4cq11pqb:i378qbvo9tookvam70e509q0et@ds131139.mlab.com:31139/heroku_4cq11pqb*/
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const { ObjectID } = require('mongodb');
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
