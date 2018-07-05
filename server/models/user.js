49e4380342796b9cae69b1864c22d1e6ca7d554b
Joseph Skrzysowski <josephskrzysowski@gmail.com>
2018-07-05 13:04:04 -0400

    Added POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -50,7 +50,6 @@ UserSchema.methods.generateAuthToken = function () {
   /*there are some inconsistencies accross diff mongoose versions so changed push() out for concat()
 	   user.tokens.push({acess, token});
 	*/
-
   return user.save().then(() => {
 	return token;
   });
@@ -71,6 +70,28 @@ UserSchema.statics.findByToken = function(token) {
 	'_id': decoded._id,
 	'tokens.token': token,
 	'tokens.access': 'auth'
+  });
+
+};
+
+UserSchema.statics.findByCredentials = function (email, password) {
+  const User = this;
+
+  return User.findOne({ email }).then((user) => {
+	if (!user) {
+	  return Promise.reject();
+	}
+
+	return new Promise((resolve, reject) => {
+	  bcrypt.compare(password, user.password, (err, res) => {
+		if (res) {
+		  resolve(user);
+		} else {
+		  reject();
+		}
+
+	  });
+	});
   });
 
 };
--- server/server.js
+++ server/server.js
@@ -155,6 +155,23 @@ app.get('/users/me', authenticate, (req,res) => {
   res.send(req.user);
 });
 
+app.post('/users/login', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password)
+	.then((user) => {
+	  return user.generateAuthToken().then((token) => {
+		res.header('x-auth', token).send(user);
+	  });
+	}).catch((e) => {
+	  res.status(400).send();
+  });
+
+
+});
+
+
+
 const server = app.listen(PORT, () => {
   console.log(`Express running on PORT, ${PORT}         →`);
 });
